40bdf42bc7a300939e8ce8e7639c603adfa21135
AlphaFlagship <vishwakarmaabhinav54@gmail.com>
2021-06-15 23:05:53 +0530

    [WIP]
    I had just understood the fundatmentals, in a couple of days i'll finish it

---
+++ CampusApp/App.tsx
@@ -0,0 +1,49 @@
+import {StatusBar} from 'expo-status-bar';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+import {Button} from 'react-native-elements';
+import comments from './ui/Comments/CommentExample';
+import CommentBox from './ui/Comments';
+
+export default function App() {
+  const [commentsVisible, setCommentsVisible] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.front}>
+        Open up App.tsx to start working on your app!
+      </Text>
+      <Button
+        onPress={() => {
+          setCommentsVisible(true);
+        }}
+        title="Open Comments"
+      />
+      {commentsVisible ? (
+        <CommentBox
+          comments={comments}
+          isVisible={true}
+          onBack={() => {
+            setCommentsVisible(false);
+          }}
+        />
+      ) : (
+        <></>
+      )}
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  front: {
+    marginBottom: 10,
+  },
+});
--- Web Dev/Projects/Calculator/script.js
+++ Web Dev/Projects/Calculator/script.js
@@ -3,6 +3,16 @@ var number1= 0;
 let test=0;
 let result;
 let firstNumber;
+let flag =0;
+let flag1 =0; 
+function allclear() {
+    let step = document.getElementById("steps");
+    step.innerHTML = `0`;
+    number1 =0;
+    test =0;
+    firstNumber =0;
+    flag =0;   
+}
 function input(value)
 {
     let number= document.getElementById("steps")
@@ -10,7 +20,9 @@ function input(value)
     number1 = enter + number1*10;
     console.log(number1);
     number.innerHTML += enter;
-    
+    if (test !=0) {
+        flag =1;
+    }
 }
 function add() {
     let step = document.getElementById("steps");
@@ -40,22 +52,80 @@ function add() {
     
 }
 function product() {
-    let step = document.getElementById("steps")
-    test =2;
-    step.innerHTML+= `*`;
+    let step = document.getElementById("steps");
+    if(test == 0){
+        let step = document.getElementById("steps");
+        test =2;
+        step.innerHTML+= `*`;
+        const temp1 = number1;
+        firstNumber = temp1;
+        console.log(number1);
+        number1 = 0;
+    }
+    
+    if (test != 0) {
+        const temp1 = firstNumber;
+        let x = document.getElementById("steps").textContent;
+        var textString =  x;
+        textString = textString.toString();
+        let newtextString=textString.slice(0,-1);
+        console.log(newtextString);
+        steps.innerHTML = newtextString;
+        test =2;
+        step.innerHTML+= `*`;
+        firstNumber = temp1;
+        number1 = 0;
+    }
+}
+function divide() {let step = document.getElementById("steps");
+if(test == 0){
+    let step = document.getElementById("steps");
+    test =3;
+    step.innerHTML+= `/`;
     const temp1 = number1;
+    firstNumber = temp1;
+    console.log(number1);
+    number1 = 0;
+}
+
+if (test != 0) {
+    const temp1 = firstNumber;
+    let x = document.getElementById("steps").textContent;
+    var textString =  x;
+    textString = textString.toString();
+    let newtextString=textString.slice(0,-1);
+    console.log(newtextString);
+    steps.innerHTML = newtextString;
+    test =3;
+    step.innerHTML+= `/`;
     firstNumber = temp1;
     number1 = 0;
 }
-function divide() {
-    let step = document.getElementById("steps")
-    test =1;
-    step.innerHTML+= `+`;
 }
-function substract() {
-    let step = document.getElementById("steps")
-    test =1;
-    step.innerHTML+= `+`;
+function substract() {let step = document.getElementById("steps");
+if(test == 0){
+    let step = document.getElementById("steps");
+    test =4;
+    step.innerHTML+= `-`;
+    const temp1 = number1;
+    firstNumber = temp1;
+    console.log(number1);
+    number1 = 0;
+}
+
+if (test != 0) {
+    const temp1 = firstNumber;
+    let x = document.getElementById("steps").textContent;
+    var textString =  x;
+    textString = textString.toString();
+    let newtextString=textString.slice(0,-1);
+    console.log(newtextString);
+    steps.innerHTML = newtextString;
+    test =4;
+    step.innerHTML+= `-`;
+    firstNumber = temp1;
+    number1 = 0;
+}
 }
 function negative() {
     let step = document.getElementById("steps")
@@ -72,5 +142,20 @@ function calculate() {
         result=firstNumber*number1;
         let solved = document.getElementById("result");
         solved.innerHTML=result;
+    }
+    else if (test ==3) {
+        result=firstNumber/number1;
+        let solved = document.getElementById("result");
+        solved.innerHTML=result;
+    }
+    else if (test ==4) {
+        result=firstNumber-number1;
+        let solved = document.getElementById("result");
+        solved.innerHTML=result;
+    }
+}
+function clear() {
+    if (test ==0 && flag ==0 ) {
+        
     }
 }
